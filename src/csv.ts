1f278d6159d774a24f324c3af552991462663055
Bernardo Belchior <bernardo.belchior1@gmail.com>
2018-09-13 17:53:04 +0100

    Remove loading functions.

--- src/csv.ts
+++ src/csv.ts
@@ -1,5 +1,3 @@
-import fs from "fs";
-import { Course, Faculty } from "./models";
 // tslint:disable-next-line no-var-requires
 const csv = require("csv");
 
@@ -13,27 +11,4 @@ export function generateCsv(objects: any): Promise<string> {
       }
     });
   });
-}
-
-function loadCsv(path: string) {
-  const data = fs.readFileSync(path).toString();
-
-  return new Promise((resolve, reject) => {
-    csv.parse(data, { columns: true }, (error: any, output: object) => {
-      if (error) {
-        reject(error);
-      }
-      console.log(error);
-
-      resolve(output);
-    });
-  });
-}
-
-export async function loadCourses(): Promise<Course[]> {
-  return loadCsv("./results/courses.csv") as Promise<Course[]>;
-}
-
-export async function loadFaculties(): Promise<Faculty[]> {
-  return loadCsv("./results/faculties.csv") as Promise<Faculty[]>;
 }
